772bfdf5cbfde125feccc7bbe643ed9a567df77d
Aaron Kim <aaron.th.kim@gmail.com>
2018-01-24 00:45:42 -0700

    master-detail pages finished, data binding to be done

---
+++ Traquest/Traquest/ViewModels/ControlRootMasterVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Traquest.Views;
+
+namespace Traquest.ViewModels
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    class ControlRootMasterVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<ControlRootMenuItem> MenuItems { get; set; }
+        public string AppName { get; set; }
+
+        public ControlRootMasterVM()
+        {
+            AppName = "Traquest";
+            MenuItems = new ObservableCollection<ControlRootMenuItem>(new[]
+            {
+                new ControlRootMenuItem { Id = 0, Title = "Home" },
+                new ControlRootMenuItem { Id = 1, Title = "Sessions" },
+                new ControlRootMenuItem { Id = 2, Title = "Test" },
+                new ControlRootMenuItem { Id = 3, Title = "Settings" },
+                new ControlRootMenuItem { Id = 4, Title = "Exit Application" },
+            });
+        }
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ Traquest/Traquest/Views/ControlRootMaster.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Traquest.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ControlRootMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public ControlRootMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new ViewModels.ControlRootMasterVM();
+            ListView = MenuItemsListView;
+        }
+
+    }
+}
